ad57369dc5c3826b45fa393aa8281de035472882
jackchen <305076862@qq.com>
2020-04-03 14:57:11 +0800

    轮播图

--- src/main.js
+++ src/main.js
@@ -3,14 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import fastclick from 'fastclick'// 解决手机点击延迟300ms问题
 import './assets/styles/reset.css'// 重置css
 import './assets/styles/border.css'// 解决手机端1px边框问题
 import './assets/styles/iconfont.css'
+import 'swiper/css/swiper.css'
+
 
 Vue.config.productionTip = false
 fastclick.attach(document.body)
-
+Vue.use(VueAwesomeSwiper)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
